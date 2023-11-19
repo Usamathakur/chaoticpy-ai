5510383149256d72517eb57a764d8a4b0c01f93b
Usamathakur <51357843+Usamathakur@users.noreply.github.com>
2023-11-19 15:10:36 +0530

    Add files via upload

---
+++ chaoticpy-ai/__init__.py
@@ -0,0 +1,4 @@
+# chaoticpy/__init__.py
+
+from .core import chaotic_numbers
+from .utils import helper_functions, constants
--- chaoticpy-ai/tests/test_core.py
+++ chaoticpy-ai/tests/test_core.py
@@ -1,13 +1,39 @@
-from chaoticpy_ai.core import ChaoticNumber
+# chaoticpy/tests/test_core.py
+import unittest
+from chaoticpy.core.chaotic_numbers import ChaoticNumber
+from chaoticpy.core.operations import order_chaos
+from chaoticpy.utils.helper_functions import calculate_mean
 
-def test_add():
-    chaotic_num1 = ChaoticNumber(2)
-    chaotic_num2 = ChaoticNumber(3)
-    result = chaotic_num1.add(chaotic_num2)
-    assert result.value == 5
+class TestChaoticNumbers(unittest.TestCase):
+    def test_addition(self):
+        # Test addition operation for chaotic numbers
+        num1 = ChaoticNumber(2, 3)
+        num2 = ChaoticNumber(4, 5)
+        result = num1.add(num2)
+        self.assertEqual(result.real, 6)
+        self.assertEqual(result.imaginary, 8)
 
-def test_multiply():
-    chaotic_num1 = ChaoticNumber(2)
-    chaotic_num2 = ChaoticNumber(3)
-    result = chaotic_num1.multiply(chaotic_num2)
-    assert result.value == chaotic_num1.value * chaotic_num2.value  # Random factor, so we check the structure
+    def test_multiplication(self):
+        # Test multiplication operation for chaotic numbers
+        num1 = ChaoticNumber(2, 3)
+        num2 = ChaoticNumber(4, 5)
+        result = num1.multiply(num2)
+        self.assertEqual(result.real, -7)
+        self.assertEqual(result.imaginary, 22)
+
+class TestOperations(unittest.TestCase):
+    def test_order_chaos(self):
+        # Test ordering function for chaos set
+        chaos_set = [...]  # Replace [...] with an actual chaotic set
+        ordered_set = order_chaos(chaos_set)
+        self.assertEqual(ordered_set, [...])  # Replace [...] with the expected ordered set
+
+class TestHelperFunctions(unittest.TestCase):
+    def test_calculate_mean(self):
+        # Test calculation of mean for chaotic set
+        chaos_set = [...]  # Replace [...] with an actual chaotic set
+        mean = calculate_mean(chaos_set)
+        self.assertAlmostEqual(mean, expected_mean, delta=0.001)  # Set the expected mean value and delta
+
+if __name__ == '__main__':
+    unittest.main()
